9e3c6b6778846e8b6d789eb220505acb24c02393
Amy Carrillo <amycarrillo29@yahoo.com>
2018-04-03 18:27:35 -0500

    express

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var htmlRouter = require("./routes/htmlRoutes");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(htmlRouter);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
